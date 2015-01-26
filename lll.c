2000555a655685de0ca992396b287d592b778e8d
jerry <jerry_chang@gemteks.com>
2015-01-26 14:41:53 +0800

    include find_node() and revise the return value

--- lll.c
+++ lll.c
@@ -9,7 +9,7 @@ struct foo {
 	struct list_head list;
 };
 
-void add_node(char *str, struct list_head *head)
+int add_node(char *str, struct list_head *head)
 {
     struct foo *fooptr = (struct foo *)malloc(sizeof(struct foo));
     assert(fooptr != NULL);
@@ -20,100 +20,99 @@ void add_node(char *str, struct list_head *head)
 //    strcpy(fooptr->word, str);
     INIT_LIST_HEAD(&fooptr->list);
     list_add_tail(&fooptr->list, head);
+    return 0;
 }
-/*
-struct foo *find_node(char *find, struct list_head *head)
+
+int find_node(char *find, struct list_head *head, struct list_head *findptr)
 {
 	struct list_head *iter;
+	struct list_head *n;
 	struct foo *node;
-	list_for_each(iter, head) {
+	list_for_each_safe(iter, n, head) {
 		node = list_entry(iter, struct foo, list);
 		if(strcmp(node->word,find) == 0) {
-			printf("find it !\n");
-			return iter;
+			findptr->next = iter;
+			return 0;
 		}
 	}
-	return NULL;
+	return 1;
 }
-*/
+
 int insert_node(char *find, char *insert, struct list_head *head)
 {
     struct foo *fooptr = (struct foo *)malloc(sizeof(struct foo));
-    struct list_head *iter;
-    struct foo *objPtr;
+    struct list_head *ptr = malloc(sizeof(struct list_head));
     assert(fooptr != NULL);
     fooptr->word = strdup(insert);
-    list_for_each(iter, head) {
-	objPtr = list_entry(iter, struct foo, list);
-	if(strcmp(objPtr->word,find) == 0) {
-		list_add(&fooptr->list,iter);
-		return 0;
-	}
+    if(find_node(find,head,ptr) == 0) {
+    	list_add(&fooptr->list,ptr->next);
+    	free(ptr);
+    	return 0;
+    } else {
+        printf("INVALID COMMAND\n");
+    	return 1;
     }
-    return 1;
-    
-}
-
+}    
 int append_node(char *find, char *append, struct list_head *head)
 {
     struct foo *fooptr = (struct foo *)malloc(sizeof(struct foo));
-    struct list_head *iter;
-    struct foo *objPtr;
-
+    struct list_head *ptr = malloc(sizeof(struct list_head));
     assert(fooptr != NULL);
     fooptr->word = strdup(append);
-    list_for_each(iter, head) {
-	objPtr = list_entry(iter, struct foo, list);
-	if(strcmp(objPtr->word,find) == 0) {
-		list_add_tail(&fooptr->list,iter);
-		return 0;
-	}
+    if(find_node(find,head,ptr) == 0) {
+    	list_add_tail(&fooptr->list,ptr->next);
+    	free(ptr);
+    	return 0;
+    } else {
+	printf("INVALID COMMAND\n");
+	return 1;
     }
-    return 1;
 }
 
 int delete_node(char *delete, struct list_head *head)
 {
+    struct list_head *ptr = malloc(sizeof(struct list_head));
     struct list_head *iter;
-    struct list_head *n;
-    struct foo *objPtr;
-
-    list_for_each_safe(iter, n, head) {
-	objPtr = list_entry(iter, struct foo, list);
-	if(strcmp(objPtr->word,delete) == 0) {
-		list_del(&objPtr->list);
-		free(objPtr->word);
-		free(objPtr);
-		return 0;
-	}
+    struct foo *objptr;
+    if(find_node(delete,head,ptr) == 0) {
+    	objptr = list_entry(ptr->next, struct foo, list);
+    	list_del(&objptr->list);
+    	free(ptr);
+    	free(objptr->word);
+    	free(objptr);
+    	return 0;
+    } else {
+	printf("INVALID COMMAND\n");
+	return 1;
     }
-    return 1;
 }
 
-void display(struct list_head *head)
+int display(struct list_head *head)
 {
     struct list_head *iter;
-    struct foo *objPtr;
+    struct foo *objptr;
 
     list_for_each(iter, head) {
-        objPtr = list_entry(iter, struct foo, list);
-        printf("%s ", objPtr->word);
+        objptr = list_entry(iter, struct foo, list);
+        printf("%s ", objptr->word);
     }
     printf("\n");
+    return 0;
 }
 
-void delete_all(struct list_head *head)
+int delete_all(struct list_head *head)
 {
     struct list_head *iter;
-    struct foo *objPtr;
+    struct foo *objptr;
     redo: 
     list_for_each(iter, head) {
-        objPtr = list_entry(iter, struct foo, list);
-        list_del(&objPtr->list);
-	free(objPtr->word);
-        free(objPtr);
+        objptr = list_entry(iter, struct foo, list);
+        list_del(&objptr->list);
+	free(objptr->word);
+        free(objptr);
 	goto redo;
     }
+    return 0;
 }
 
 
@@ -121,9 +120,10 @@ void main()
 {
 	//char string[1024];
 	char string[1024];
-	char *s;
+	char *token = NULL;
+	char *saveptr = NULL;
 	char *put[100];
-	int s_count = 0;
+	int token_count = 0;
 	int word_count = 0;
 //	int x;
     	LIST_HEAD(fooHead);
@@ -132,40 +132,46 @@ void main()
 		if(string[strlen(string) - 1] == '\n') {
 			string[strlen(string) - 1] = '\0';
 
-			s = strtok(string," ");
-			while(s != NULL) {
-				put[s_count++] = s;
-				s = strtok(NULL," ");
+			token = strtok_r(string," ",&saveptr);
+			while(token != NULL) {
+				put[token_count++] = token;
+				token = strtok_r(NULL," ",&saveptr);
 			}
-			if(s_count > 20) {
+			if(token_count > 20) {
 				printf("String token is overflow. The following commands are not available.\n");
 			}
 			else {
 				if(strcmp(put[0],"a") == 0) {
 					//find_node(put[1],&fooHead);
-					append_node(put[1],put[2],&fooHead);
+					if(append_node(put[1],put[2],&fooHead)) {
+						break;
+					}
 					display(&fooHead);
 				}
 				else if(strcmp(put[0],"i") == 0) {
-					insert_node(put[1],put[2],&fooHead);
+					if(insert_node(put[1],put[2],&fooHead)) {
+						break;
+					}
                         		display(&fooHead);
 				}
 				else if(strcmp(put[0],"d") == 0) {
-					delete_node(put[1],&fooHead);
+					if(delete_node(put[1],&fooHead)) {
+						break;
+					}
 					display(&fooHead);
 				}
 				else {
 					delete_all(&fooHead);
-					while(word_count != s_count) {
+					while(word_count != token_count) {
 						add_node(put[word_count],&fooHead);
 						word_count++;
 					}
 					display(&fooHead);
 				}
-				s_count = 0;
+				token_count = 0;
 				word_count = 0;
 			}
-			s_count = 0;
+			token_count = 0;
 		}
 		else 
 			printf("Your input string overflow. The following commands are not available.\n ");
