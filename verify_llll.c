451729188d2e04bff7acf3013d13b0a436b8c99f
jerry <jerry_chang@gemteks.com>
2015-02-04 13:48:25 +0800

    revise the buffer for input and out

--- verify_llll.c
+++ verify_llll.c
@@ -2,47 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 int main(int argc, char *argv[]) {
-//	FILE *inFile;
-	FILE *outFile;
-//	char inbuffer[1024];
-	char string[1024];
-	char outbuffer[1024];
-	int ptr;
-	int flag1=0;
-	int flag2=0;
-/*
-	inFile = fopen("input.txt","r");
-	if(inFile == NULL) {
-		printf("InputFile Open Failure");
-	}
-*/	
-	outFile = fopen(argv[1],"r");
-	if(!outFile) {
+	FILE *outfile;
+	char *string;
+	char *outbuffer;
+	int compare;
+	int input_flag = 0;
+	int output_flag = 0;
+	size_t in_len = 0;
+	size_t out_len = 0;
+	ssize_t input_read;
+	ssize_t output_read;
+	outfile = fopen(argv[1],"r"); // open output file
+	if(!outfile) {
 		printf("The file : %s is not found!\n",argv[1]);
 		return;
 	}
 	do{
 		
-		if(fgets(string,1024,stdin) == NULL) {
-			flag1 = 1;
+		if((input_read = getline(&string,&in_len,stdin)) == -1) {
+			input_flag = 1;
 		} else {
+			char *in_end = strchr(string,'\n'); // find new line of input
+			strncpy(in_end,"\0",1);
 		}
-		if(fgets(outbuffer,1024,outFile) == NULL) {
-			flag2 = 1;
+		if((output_read = getline(&outbuffer,&out_len,outfile)) == -1) {
+			output_flag = 1;
 		} else {
+			char *out_end = strchr(outbuffer,'\n'); // find new line of output
+			strncpy(out_end,"\0",1); 
 		}
-		if(flag1 == 1 && flag2 == 1) {
+		if(input_flag == 1 && output_flag == 1) { // while finish reading
 			printf("OK\n");
 			break;
 		}
-		ptr = strcmp(string,outbuffer);
-		//printf("%d\n",ptr);
-		if((ptr) != 0) {
+		compare = strcmp(string,outbuffer);
+		if(compare != 0) {
 			printf("Fail\n");
-			printf("input = %soutput = %s\n",string,outbuffer);
+			printf("input = %s\noutput = %s\n",string,outbuffer);
 			break;
 		}
 	} while(1);
-	fclose(outFile);
+	fclose(outfile);
 	return 0;
 }
